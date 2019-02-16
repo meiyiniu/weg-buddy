5a958474fdf6eb1368aed5fe0c6fc48cb71ba82d
Michael Qin <michael.qin@uwaterloo.ca>
2019-02-16 18:57:21 -0500

    testing wegmans

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template("page.html")
+    #return render_template("page.html")
+    return render_template("apiTesting.html")
+
 
 # Gets text from the webpage's textbox
 @app.route('/', methods=['INPUT'])
 def get_user_response():
     return request.form['text']
+
+@app.route('/wegmans')
+def wegmans():
+    return render_template("apiTesting.html")
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 4428, debug=True)
