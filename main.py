bcc26804fa22519f4cb0a58d2d0692f16eda4f27
Sam Lowe <samlowe106@gmail.com>
2019-02-16 17:56:36 -0500

    Basic webpage and input implemented

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-from flask import Flask
-
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "<h1>WegBuddy</h1>"
+    return render_template("page.html")
 
-@app.route("/test")
-def test():
-    return "test message"
+# Gets text from the webpage's textbox
+@app.route('/', methods=['INPUT'])
+def get_user_response():
+    return request.form['text']
 
 if __name__ == '__main__':
     app.run(host = "0.0.0.0", port = 4428, debug=True)
