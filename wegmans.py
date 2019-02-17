6cde88c78f2ae0131acfbeda1c0c4f14754bf35d
Reid Campolong <reidcampolong@gmail.com>
2019-02-17 12:02:01 -0500

    Added recipe get from API

--- wegmans.py
+++ wegmans.py
@@ -3,7 +3,7 @@ import json
 
 key = "1472762928494a6da6b27184b02a6af8";
 
-def grabProduct(ID):
+def queryAPIForProduct(ID):
     get_url = 'https://api.wegmans.io/products/{sku}?api-version=2018-10-18'
     get_url = get_url.replace('{sku}', str(ID))
     headers = {
@@ -12,6 +12,24 @@ def grabProduct(ID):
     r = requests.get(get_url, headers=headers)
     rd = json.loads(r.text)
 
-    print('Request data ' + str(rd))
+    toReturn = str(rd['name']) + ":" + str(rd['tradeIdentifiers'][0]['images'])
 
-grabProduct(11914)
+    print('Request data ' + toReturn)
+
+def queryAPIForRecipe(ID) -> str:
+    get_url = 'https://api.wegmans.io/meals/recipes/{id}?api-version=2018-10-18'
+    get_url = get_url.replace('{id}', str(ID))
+    headers = {
+        'Subscription-Key': key,
+    }
+    r = requests.get(get_url, headers=headers)
+    rd = json.loads(r.text)
+
+    toReturn = str(rd['name']) + ":" + str(rd['_links'][2]['href'])
+
+    return toReturn
+
+#def getProduct(ID):
+
+
+queryAPIForRecipe(20402)
